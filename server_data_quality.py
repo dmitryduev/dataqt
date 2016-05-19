e211ee118c46deec304d207919b8f43da08c2b39
Dmitry Duev <dmitry.duev@gmail.com>
2016-05-19 13:23:31 -0700

    display FITS header in modal.

--- server_data_quality.py
+++ server_data_quality.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 import os
 import json
 import datetime
 import ConfigParser
 import inspect
 from collections import OrderedDict
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, jsonify  # send_from_directory
 from flask.ext.basicauth import BasicAuth
 app = Flask(__name__)
 
@@ -36,6 +37,31 @@ def hw():
     return render_template('template-main.html', dates=dates)
 
 
+#
+@app.route('/_get_fits_header')
+def get_fits_header():
+    date = request.args.get('date', 0, type=str)
+    source = request.args.get('source', 0, type=str)
+
+    f_json_sci = os.path.join(path_to_website_data, '{:s}'.format(date), '{:s}.json'.format(date))
+
+    if os.path.exists(f_json_sci):
+        with open(f_json_sci) as fjson_sci:
+            data = json.load(fjson_sci, object_pairs_hook=OrderedDict)
+            data = OrderedDict(data)
+    else:
+        # if failed:
+        return jsonify(result={})
+
+    for pot in ('high_flux', 'faint'):
+        header = [s['header'] for s in data[pot] if source in s['header']['FILENAME'][0]]
+        if len(header) > 0:
+            return jsonify(result=OrderedDict(header[0]))
+
+    # if failed:
+    return jsonify(result={})
+
+
 # serve particular date
 @app.route('/<int:date>')
 @basic_auth.required
@@ -54,14 +80,14 @@ def show_date(date):
         # sci data
         if os.path.exists(f_json_sci):
             with open(f_json_sci) as fjson_sci:
-                data = json.load(fjson_sci)
+                data = json.load(fjson_sci, object_pairs_hook=OrderedDict)
                 data = OrderedDict(data)
         else:
             data = False
         # seeing
         if os.path.exists(f_json_seeing):
             with open(f_json_seeing) as fjson_seeing:
-                seeing = json.load(fjson_seeing)
+                seeing = json.load(fjson_seeing, object_pairs_hook=OrderedDict)
                 seeing = OrderedDict(seeing)
         else:
             seeing = False
